586a1c8dc021eea6a9f96d9cd8ce4f21cf4bbf12
PoC-Yeh <allisonxp7@gmail.com>
2017-09-15 15:17:26 +0800

    Rename main.py to crawl.py

--- crawl.py
+++ crawl.py
@@ -8,27 +8,29 @@ import pickle
 from get_serp import *
 
 
-def google_crawl(url, filename):
-    bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength) 
-    serp_list = []
+url = "https://www.google.com.tw/search?q=%E5" #search query of the keyword
+page_count = 0
+bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength) 
+serp_list = []
 
-    while True:
-        text = requests.get(url).text
-        soup = BeautifulSoup(text, "html.parser")
-
-        get_title(soup, serp_list)
-        url = next_page(soup)
-        if url != None:
-            page_count += 1
-            bar.update(page_count)
-            if page_count % 4 == 0:
-                time.sleep(60*1)
-        else:
-            break
-            
-    f = open('google_serp_{}.csv'.format(filename), 'w')
-    w3 = csv.writer(f)
-    w3.writerows(serp_list)
+while True:
+    text = requests.get(url).text
+    soup = BeautifulSoup(text, "html.parser")
     
-    with open("google_serp_{}_pickle.txt".format(filename), "wb")as c:
-        pickle.dump(serp_list, c)
+    get_title(soup, serp_list)
+    url = next_page(soup)
+    if url != None:
+        page_count += 1
+        bar.update(page_count)
+        if page_count % 4 == 0:
+            time.sleep(60*1)
+    else:
+        break
+            
+            
+f = open('google_serp.csv', 'w')
+w3 = csv.writer(f)
+w3.writerows(serp_list)
+
+with open("google_serp_pickle.txt", "wb")as c:
+    pickle.dump(serp_list, c)
